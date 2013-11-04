5f39ae7486b927b757d1a9b41bfcc2db4e4f2521
mrtowel <przemyslaw.bis@gmail.com>
2013-11-04 01:38:47 +0100

    welcome message modified

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   	@time = Time.now
+  	@user = current_user.nil? ? "Stranger" : current_user.email.split('@')[0]
   end
 
   def help
-  	@text = "Don't botherś!"
+  	@text = "Don't bother!"
   end
 end
