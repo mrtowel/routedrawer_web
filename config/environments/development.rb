5898b9e89afdcd711c00e0d039cddb7505e88f2a
Przemek Bis <przemyslaw.bis@gmail.com>
2013-11-07 22:11:13 +0100

    production file fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,12 +25,13 @@ Managable::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  # config.assets.debug = true
 
-  config.assets.enabled = true
+  # config.assets.enabled = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+  config.assets.compile = true
 
 end
