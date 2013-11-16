c79b06d69be0a9062e59106fdc89ae19b1309282
Przemek Bis <przemyslaw.bis@gmail.com>
2013-11-16 15:32:19 +0100

    mongo changed

---
+++ out/test/manager/requests/locations_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Locations" do
+  describe "POST /locations" do
+    it "works!" do
+      post location_path, format: 'application/json'
+      response.status.should be(200)
+    end
+  end
+end
